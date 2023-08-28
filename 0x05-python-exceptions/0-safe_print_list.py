d2c928686fcd44183ace09ccafc3fcbdde7db471
Liam190100 <jnnjuguna8@gmail.com>
2023-08-28 07:55:28 -0700

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=" ")
             number += 1
-    except:
+    except ValueError:
         pass
     finally:
         print()
