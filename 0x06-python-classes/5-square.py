9f48150d9305880514b3875ae14ac1ee940074db
root <Liam190100>
2023-08-30 16:39:03 +0000

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,50 +1,26 @@
 #!/usr/bin/python3
 """
-This module defines a Square class that represents a square shape.
-
-The Square class has methods to set and retrieve the size of the square,
-calculate the area, and print a visual representation of the square using '#'.
+This module defines a class Square with a private instance
+attribute size
 """
+
 
 class Square:
     """
-    Represents a square with a size attribute and related methods.
-    
-    Attributes:
-        size (int): The size of the square's sides.
-    """
+    This class represents a class square with a size"""
 
-	def __init__(self, size=0):
-        """
-        Initialize a new Square instance with a specified size.
+    def __init__(self, size=0):
+        """This square with the given size"""
+        self.size = size
 
-        Args:
-            size (int): The size of the square's sides. Default is 0.
-        """
-	self.size = size
-
-	@property
-	def size(self):
-        """
-        Get the size of the square.
-
-        Returns:
-            int: The size of the square's sides.
-        """
+    @property
+    def size(self):
+        """This method returns the size square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square.
-
-        Args:
-            value (int): The size to set.
-
-        Raises:
-            TypeError: If the provided value is not an integer.
-            ValueError: If the provided value is less than 0.
-        """
+        """This method sets the size square"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -52,23 +28,13 @@ class Square:
         self.__size = value
 
     def area(self):
-        """
-        Calculate the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
+        """This method returns the square"""
         return self.__size ** 2
 
     def my_print(self):
-        """
-        Print a visual representation of the square using '#'.
-
-        If the size is 0, an empty line is printed.
-        """
+        """This method prints the square # in stdout"""
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
+            for i in range(self.__size):
                 print("#" * self.__size)
-
