59ec83587bbd7450c4912b2a819b30f118f88ffa
Liam190100 <jnnjuguna8@gmail.com>
2023-09-12 04:44:56 -0700

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""
-Define class Square
-"""
+"""A child class Square"""
 
 
-from __import__('9-rectangle') import Rectangle
+class Square (Rectangle):
 
-class Square(Rectangle):
-
-    """A child class square"""
+    """A child class that represents a square"""
 
     def __init__(self, size):
         """Initializes a square with a given size"""
-        # Validate size
+        # Validate size as a positive integer
         self.integer_validator("size", size)
-
         self.__size = size
 
         super().__init__(size, size)
 
     def area(self):
-        """Return the area square"""
+        """Return the area of the square"""
         return self.__size ** 2
 
     def __str__(self):
-        """Return a string square"""
+        """Return a string representing the square"""
         return "[Square] {}/{}".format(self.__size, self.__size)
