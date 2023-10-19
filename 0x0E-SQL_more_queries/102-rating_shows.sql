3d68108d5ef30b7c9f9672027f5c192e95314117
Liam190100 <jnnjuguna8@gmail.com>
2023-10-19 09:31:07 -0700

    rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+--  lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- display: tv_shows.title - rating sum.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
