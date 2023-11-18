ee7d9422e35b244204a4536690d69a911ee7a29f
Liam190100 <jnnjuguna8@gmail.com>
2023-11-18 08:55:20 -0800

    Mysqlalchemy

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""write a script that takes in arguments
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+But this time, write one that is safe from MySQL injections!"""
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+if __name__ == '__main__':
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python 3
+"""The script that takes in the name of a state as an argument
+lists all cities of that state
+using the database hbtn_0e_4_usa"""
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    db.close()
+if __name__ == '__main__':
