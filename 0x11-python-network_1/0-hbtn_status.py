57ab0a03e48cafc263d237ccfa28b14a735b4b9f
Liam190100 <jnnjuguna8@gmail.com>
2023-12-01 08:48:07 -0800

    Http

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script Fetches https://intranet.hbtn.io/status
+Use the package urllib
+"""
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+    as respon:
+        html = respon.read()
+
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+
+if __name__ == "__main__":
+    close()
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+URL on the 0x11-python-network_1
